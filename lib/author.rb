2f68f13e89247332132a22f11948d69f0233fb62
Knika Dass <knikadass@gmail.com>
2020-07-30 22:56:45 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,23 +9,24 @@ class Author
   end
 
   def posts
-  @@posts
-end
+    Post.all.select {|post| post.author = self}
+  end
+
+  def add_post(post)
+    @@post_count += 1
+    @posts << post
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    @@post_count += 1
+    p = Post.new(title)
+    self.add_post(p)
+    p
+  end
 
   def self.post_count
     @@post_count
   end
 
-  def add_post(post_title)
-    @posts << post_title
-    post_title.author = self
-    @@post_count += 1
-  end
-
-  def add_post_by_title(post_title)
-    post = Post.new(post_title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
-end 
+end
